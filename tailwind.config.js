ad46c39aed4e984d589ce1aaa3452750927929af
parlabarbedeMerlin <tristan.tr.renard@gmail.com>
2024-05-02 13:26:43 +0200

    add eslint config & patch linter errors

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,11 +8,11 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        primary: 'rgb(220, 121, 100)',
-        secondary: 'rgb(125, 122, 188)',
-        tertiary: 'rgb(231, 221, 201)',
-        quaternary: 'rgb(27, 25, 21)',
-        quinary: 'rgb(238, 233, 222)',
+        primary: "rgb(220, 121, 100)",
+        secondary: "rgb(125, 122, 188)",
+        tertiary: "rgb(231, 221, 201)",
+        quaternary: "rgb(27, 25, 21)",
+        quinary: "rgb(238, 233, 222)",
       },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
@@ -22,4 +22,4 @@ module.exports = {
     },
   },
   plugins: [],
-};
+}
