3060a805fc08bfb7cd328ab4e75b31647c96b4d6
Léa Schmitt <leaschmitt@cbo?.home>
2024-05-05 14:25:01 +0200

    homepage in english

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -11,18 +11,18 @@ export default function Home() {
       <NavBar />
       <main>
         <div className="flex">
-          <img src="/heroheader.png" alt="Image header" className="w-2/3" />
+          <img src="/heroheader.png" alt="Header Image" className="w-2/3" />
           <div className="flex flex-col justify-center">
             <h1 className="uppercase text-6xl font-black text-primary mb-10">
               SKILLPORT
             </h1>
             <div className="text-xl">
               <p>
-                Montrez vos{" "}
-                <span className="font-bold text-primary">skill</span>
+                Showcase your{" "}
+                <span className="font-bold text-primary">skills</span>
               </p>
               <p>
-                Créez votre{" "}
+                Create your{" "}
                 <span className="font-bold text-primary">portfolio</span>
               </p>
             </div>
@@ -30,68 +30,68 @@ export default function Home() {
         </div>
         <div className="flex flex-col items-center mt-20">
           <p className="text-center w-3/5">
-            Avoir un portfolio en ligne est essentiel dans un monde où la
-            première impression compte énormément. C'est l'occasion idéale de
-            mettre en avant vos réalisations passées, vos projets réussis et vos
-            compétences uniques, créant ainsi une image professionnelle et
-            convaincante auprès des recruteurs et des clients.
+            Having an online portfolio is essential in a world where first
+            impressions matter greatly. It's the perfect opportunity to showcase
+            your past achievements, successful projects, and unique skills,
+            creating a professional and compelling image for recruiters and
+            clients.
           </p>
           <Link
             href="createFolio.jsx"
             className="text-quaternary bg-primary p-3 rounded-full mt-7"
           >
-            Créer un portfolio
+            Create a portfolio
           </Link>
         </div>
         <div className="mt-28">
           <h2 className="w-full mb-16 text-center text-4xl font-black text-primary">
-            Nos avantages
+            Our advantages
           </h2>
           <ul className="flex justify-around px-36">
             <li className="w-1/6 px-3 py-8 bg-primary rounded-lg text-center">
-              Des composants à la carte
+              Customizable components
             </li>
             <li className="w-1/6 px-3 py-8 bg-primary rounded-lg text-center">
-              Un contenu personnalisé à votre activité
+              Personalized content tailored to your activity
             </li>
             <li className="w-1/6 px-3 py-8 bg-primary rounded-lg text-center">
-              Des images pour illuser vos contenu
+              Images to illustrate your content
             </li>
             <li className="w-1/6 px-3 py-8 bg-primary rounded-lg text-center">
-              Un design sobre pour sublimer vos idées
+              Clean design to enhance your ideas
             </li>
           </ul>
         </div>
         <div className="mt-28 flex flex-col items-center">
           <h2 className="w-full mb-8 text-center text-4xl font-black text-primary">
-            L'équipe
+            The team
           </h2>
           <div className="grid grid-cols-2 grid-rows-3 w-2/5 my-8">
             <img src="/vincent.jpg" alt="Vincent" />
             <div className="flex flex-col justify-center items-center text-center">
               <p className="font-bold text-primary text-3xl">Vincent</p>
               <ul>
-                <li>Ingénieur système et réseau</li>
-                <li>Aime l'escalade</li>
-                <li>Joue à des jeux</li>
+                <li>System and network engineer</li>
+                <li>Enjoys climbing</li>
+                <li>Plays games</li>
               </ul>
             </div>
             <div className="flex flex-col justify-center items-center text-center">
               <p className="font-bold text-primary text-3xl">Léa</p>
               <ul>
-                <li>Developpeur</li>
-                <li>Aime les voitures qui vont vite</li>
-                <li>Ont lui a volé une glace</li>
+                <li>Developer</li>
+                <li>Enjoys fast cars</li>
+                <li>Had her ice cream stolen once</li>
               </ul>
             </div>
-            <img src="/lea.jpg" alt="Vincent" />
-            <img src="/tristan.jpg" alt="Vincent" />
+            <img src="/lea.jpg" alt="Léa" />
+            <img src="/tristan.jpg" alt="Tristan" />
             <div className="flex flex-col justify-center items-center text-center">
               <p className="font-bold text-primary text-3xl">Tristan</p>
               <ul>
-                <li>Developpeur</li>
-                <li>Aime la mode</li>
-                <li>Dis des mots cailloux</li>
+                <li>Developer</li>
+                <li>Enjoys fashion</li>
+                <li>Says quirky things</li>
               </ul>
             </div>
           </div>
@@ -101,7 +101,7 @@ export default function Home() {
         <Link href="https://github.com/parlabarbedeMerlin/skillport">
           GitHub
         </Link>
-        <p> © mai 2024</p>
+        <p className="ml-1"> © May 2024</p>
       </footer>
     </div>
   )
