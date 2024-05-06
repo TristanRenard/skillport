498b6ff94ab661183d2be53faf369661a4dcb1d2
VGauthieer <vincent.gauthier@supdevinci-edu.fr>
2024-05-06 09:52:55 +0200

    test_1

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "rgb(220, 121, 100)",
+        secondary: "rgb(125, 122, 188)",
+        tertiary: "rgb(231, 221, 201)",
+        quaternary: "rgb(27, 25, 21)",
+        quinary: "rgb(238, 233, 222)",
+      },
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+    },
+  },
+  plugins: [],
+}
